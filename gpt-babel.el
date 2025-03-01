3f33d9905d282b190cd2df67c7998739285b7ed8
Elle Najt <lnajt4@gmail.com>
2025-03-01 16:03:33 -0500

    Fix names, oops

--- gpt-babel.el
+++ gpt-babel.el
@@ -23,7 +23,7 @@
 (require 'general)
 (require 'gptel)
 
-(defun gptel-babel/copy-org-babel-block-and-results ()
+(defun gpt-babel/copy-org-babel-block-and-results ()
   "Copy the current org babel source block and its results."
   (interactive)
   (when (org-in-src-block-p)
@@ -40,7 +40,7 @@
        end)
       (message "Copied source block and results"))))
 
-(defun gptel-babel/init-cell-errors-gptel ()
+(defun gpt-babel/init-cell-errors-gptel ()
   "Create a new gptel buffer for cell errors."
   (interactive)
   (funcall-interactively #'gptel "*CELL ERRORS*"))
@@ -49,7 +49,7 @@
   "Please explain the issue, and please provide a corrected version of this, in a #+begin_src %s block.\n"
   "Template for the failure prompt, where %s is replaced with the language.")
 
-(defun gptel-babel/get-second-src-block (lang)
+(defun gpt-babel/get-second-src-block (lang)
   "Get content of second source block of type LANG."
   (save-excursion
     (goto-char (point-min))
@@ -62,19 +62,19 @@
           (re-search-forward "^[ \t]*#\\+end_src" nil t)
           (buffer-substring-no-properties start (match-beginning 0)))))))
 
-(defun gptel-babel/2nd-block ()
+(defun gpt-babel/2nd-block ()
   (interactive)
   (message (get-second-python-block)))
 
 
-(defun gptel-babel/org-babel-get-src-block-end ()
+(defun gpt-babel/org-babel-get-src-block-end ()
   "Get the end position of the current source block."
   (save-excursion
     (org-babel-goto-src-block-head)
     (re-search-forward "#\\+end_src" nil t)
     (line-beginning-position)))
 
-(defun gptel-babel/send-block-to-gptel ()
+(defun gpt-babel/send-block-to-gptel ()
   "Send org babel block with results to new gptel buffer without affecting current buffer."
   (interactive)
   (when (org-in-src-block-p)
@@ -94,7 +94,7 @@
            (prompt (format custom-block-failure-prompt-template lang))
 
            (buf (or (get-buffer "*CELL ERRORS*")
-                    (progn (gptel-babel/init-cell-errors-gptel)
+                    (progn (gpt-babel/init-cell-errors-gptel)
                            (get-buffer "*CELL ERRORS*")))))
 
       (with-current-buffer buf
@@ -103,7 +103,7 @@
         (gptel-send)
         (pop-to-buffer buf)))))
 
-(defun gptel-babel/diff-strings (str1 str2)
+(defun gpt-babel/diff-strings (str1 str2)
   "Return a diff between STR1 and STR2 as a string."
   (with-temp-buffer
     (let ((file1 (make-temp-file "diff1"))
@@ -115,7 +115,7 @@
       (delete-file file2)
       (buffer-string))))
 
-(defun gptel-babel/patch-gptel-blocks ()
+(defun gpt-babel/patch-gptel-blocks ()
   "Send block to gptel and show diff with accept option."
   (interactive)
   (let* ((original-buffer (current-buffer))
@@ -127,15 +127,15 @@
                                             (org-babel-goto-src-block-head)
                                             (forward-line 1)
                                             (point)))
-                                   (end (gptel-babel/org-babel-get-src-block-end)))
+                                   (end (gpt-babel/org-babel-get-src-block-end)))
                                (buffer-substring-no-properties start end))))
          (gpt-block (with-current-buffer "*CELL ERRORS*"
-                      (gptel-babel/get-second-src-block lang)))
+                      (gpt-babel/get-second-src-block lang)))
          (diff-buffer (get-buffer-create "*GPT Block Diff*"))
          (map (make-sparse-keymap)))
     (with-current-buffer diff-buffer
       (erase-buffer)
-      (insert (gptel-babel/diff-strings original-content gpt-block))
+      (insert (gpt-babel/diff-strings original-content gpt-block))
       (diff-mode)
       (define-key map (kbd "C-c C-c")
                   (lambda ()
@@ -146,7 +146,7 @@
                       (org-babel-remove-result)
                       (re-search-forward "#\\+begin_src.*$")
                       (forward-line 1)
-                      (delete-region (point) (gptel-babel/org-babel-get-src-block-end))
+                      (delete-region (point) (gpt-babel/org-babel-get-src-block-end))
                       (insert gpt-block)
                       (quit-window)
                       (pop-to-buffer original-buffer)
@@ -160,24 +160,24 @@
       (display-buffer diff-buffer)
       (pop-to-buffer diff-buffer))))
 
-(defvar gptel-babel/gptel-fix-block-buffer nil
+(defvar gpt-babel/gptel-fix-block-buffer nil
   "Buffer to patch after GPT response.")
 
-(defun gptel-babel/gptel-fix-block-response (beg end)
+(defun gpt-babel/gptel-fix-block-response (beg end)
   "Handle GPT response for block fixing."
-  (when gptel-babel/gptel-fix-block-buffer
-    (with-current-buffer gptel-babel/gptel-fix-block-buffer
-      (gptel-babel/patch-gptel-blocks))
+  (when gpt-babel/gptel-fix-block-buffer
+    (with-current-buffer gpt-babel/gptel-fix-block-buffer
+      (gpt-babel/patch-gptel-blocks))
     (pop-to-buffer "*GPT Block Diff*")
-    (setq gptel-babel/gptel-fix-block-buffer nil)))
+    (setq gpt-babel/gptel-fix-block-buffer nil)))
 
-(add-hook 'gptel-post-response-functions #'gptel-babel/gptel-fix-block-response)
+(add-hook 'gptel-post-response-functions #'gpt-babel/gptel-fix-block-response)
 
-(defun gptel-babel/gptel-fix-block ()
+(defun gpt-babel/gptel-fix-block ()
   "Send block to GPT and patch when response is received."
   (interactive)
-  (setq gptel-babel/gptel-fix-block-buffer (current-buffer))
-  (gptel-babel/send-block-to-gptel))
+  (setq gpt-babel/gptel-fix-block-buffer (current-buffer))
+  (gpt-babel/send-block-to-gptel))
 
 
 ;;; Automation:
@@ -189,7 +189,7 @@
     (shell . "Shell Error")
     (bash . "Shell Error")))
 
-(defun gptel-babel/org-src-block-results-end (src-block)
+(defun gpt-babel/org-src-block-results-end (src-block)
   (save-excursion
     (goto-char (org-element-begin src-block))
     (when-let (results-loc (org-babel-where-is-src-block-result))
@@ -198,7 +198,7 @@
       (skip-chars-backward " \t\n")
       (point))))
 
-(defun gptel-babel/check-babel-result-error ()
+(defun gpt-babel/check-babel-result-error ()
   "Check if current src block results contain an error pattern."
   (interactive)
   (save-excursion
@@ -206,35 +206,38 @@
            (lang (org-element-property :language src-block))
            (error-pattern (cdr (assoc (intern lang) org-babel-error-patterns)))
            (results-start (org-babel-where-is-src-block-result))
-           (results-end (gptel-babel/org-src-block-results-end src-block)))
+           (results-end (gpt-babel/org-src-block-results-end src-block)))
       (when (and results-start error-pattern)
         (goto-char results-start)
         (re-search-forward error-pattern results-end t)))))
 
 
-(defun gptel-babel/test-check-traceback ()
+(defun gpt-babel/test-check-traceback ()
   "Test the traceback detection function."
   (interactive)
-  (let ((has-traceback (gptel-babel/check-babel-result-error )))
+  (let ((has-traceback (gpt-babel/check-babel-result-error )))
     (message (if has-traceback "Traceback detected" "No traceback found"))))
 
 ;; Default behavior nil
-(setq gptel-babel/auto-send-on-traceback nil)
+(defcustom gpt-babel/auto-send-on-traceback nil
+  "Whether to automatically send code to GPT on traceback."
+  :type 'boolean
+  :group 'gpt-babel)
 
-(defun gptel-babel/test-for-errors (orig-fun params &rest var )
-  (if (and (fboundp 'gptel-babel/send-block-to-gptel) gptel-babel/auto-send-on-traceback)
-      (if (gptel-babel/check-babel-result-error)
-          (gptel-babel/send-block-to-gptel) ())))
+(defun gpt-babel/test-for-errors (orig-fun params &rest var )
+  (if (and (fboundp 'gpt-babel/send-block-to-gptel) gpt-babel/auto-send-on-traceback)
+      (if (gpt-babel/check-babel-result-error)
+          (gpt-babel/send-block-to-gptel) ())))
 
 
-(advice-add 'org-babel-insert-result :after #'gptel-babel/test-for-errors)
+(advice-add 'org-babel-insert-result :after #'gpt-babel/test-for-errors)
 
 
 ;;;; Shell
 ;;  Some means to get a recognizable error string in the shell output, for automatic traceback.
 ;; TODO Currently broken
 
-(defun gptel-babel/add-error-trap (orig-fun body params)
+(defun gpt-babel/add-error-trap (orig-fun body params)
   "Add ERR trap to shell scripts for error reporting."
   ;;  TODO the problem appears to be that ' is getting converted into ’  
   (let* ((trap-cmd "trap 'rc=$?; [ $rc -ne 0 ]; then echo \"Shell Error\"; fi' ERR")
@@ -248,7 +251,7 @@
                  params)
       (funcall orig-fun body params))))
 
-;; (advice-add 'org-babel-execute:shell :around #'gptel-babel/add-error-trap)
+;; (advice-add 'org-babel-execute:shell :around #'gpt-babel/add-error-trap)
 
 ;;; Keybindings
 
